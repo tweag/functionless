25a794f0e97e62f01299c280bfd2a5c62e9c478c
Felix Raimundo <felix.raimundo@tweag.io>
2017-02-27 17:04:06 +0100

    Remove useless files.

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import io.tweag.functionless.Entrypoint;
-
-class Main {
-    public static void main(String args[]) {
-	System.out.println(new Entrypoint().handleRequest("coucou", null));
-    }
-}
