752e0bef84b156da99f2352c1df7d81ae42e15f3
Felix Raimundo <felix.raimundo@tweag.io>
2017-02-28 12:00:57 +0100

    Void example working with double execution

---
+++ examples/void_example/VoidExample.hs
@@ -0,0 +1,18 @@
+-- | Sample Functionless app without arguments.
+
+{-# LANGUAGE DataKinds #-}
+
+module VoidExample where
+
+import Language.Java (J(..), JType(..), reify, reflect)
+
+foreign export ccall "Java_io_tweag_functionless_Entrypoint_voidExample" voidExample
+  :: J ('Class "java.lang.Void")
+  -> IO (J ('Class "java.lang.Void"))
+
+voidExample
+  :: J ('Class "java.lang.Void")
+  -> IO (J ('Class "java.lang.Void"))
+voidExample v = do
+    print "hello"
+    return v
