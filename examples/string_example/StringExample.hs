8db3693ddeee90841da52653b3051eee99d2af07
Felix Raimundo <felix.raimundo@tweag.io>
2017-03-03 18:35:27 +0100

    Remove JNI C glue

--- examples/string_example/StringExample.hs
+++ examples/string_example/StringExample.hs
@@ -6,18 +6,23 @@ module StringExample where
 
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
-import Language.Java (J(..), JType(..), reify, reflect)
+import Language.Java (J(..), JNIEnv, JClass, JType(..), reify, reflect)
+import Foreign.Ptr
 
-foreign export ccall "string_example" stringExample
-  :: J ('Class "java.lang.String")
+foreign export ccall "Java_io_tweag_functionless_Entrypoint_stringExample" stringExample
+  :: Ptr JNIEnv
+  -> JClass
+  -> J ('Class "java.lang.String")
   -> J ('Class "com.amazonaws.services.lambda.runtime.Context")
   -> IO (J ('Class "java.lang.String"))
 
 stringExample
-  :: J ('Class "java.lang.String")
+  :: Ptr JNIEnv
+  -> JClass
+  -> J ('Class "java.lang.String")
   -> J ('Class "com.amazonaws.services.lambda.runtime.Context")
   -> IO (J ('Class "java.lang.String"))
-stringExample input _ = do
+stringExample _ _ input _ = do
     txt <- reify input
     Text.putStrLn txt
     let newText = Text.concat [txt, Text.pack ". Hello to you too god sir"]
