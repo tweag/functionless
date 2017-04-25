9a83b1d12f3237a1d3392a8d829759c384a5b200
Mathieu Boespflug <m@tweag.io>
2017-04-25 15:46:30 +0200

    Workin java stream interface

--- src/main/java/io/tweag/functionless/Entrypoint.java
+++ src/main/java/io/tweag/functionless/Entrypoint.java
@@ -10,29 +10,25 @@ import com.amazonaws.services.lambda.runtime.Context;
 import io.tweag.jarify.HaskellLibraryLoader;
 
 public class Entrypoint implements RequestStreamHandler {
-  private static Boolean haskellInitialized = false;
+	// XXX: Make this synchronized.
+	private static Boolean haskellInitialized = false;
 
-  public static void handler(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+	@Override
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
 		// Setup the Haskell RTS if needed.
-    if (! haskellInitialized) {
-      HaskellLibraryLoader.loadLibraries();
-      haskellInitialized = true;
-    }
+		if (! haskellInitialized) {
+			HaskellLibraryLoader.loadLibraries();
+			haskellInitialized = true;
+		}
 
 		// Empty the InputStream in order to hide the Stream to the native code.
-		// TODO: find an appropriate encoding
 		byte[] input = IOUtils.toByteArray(inputStream);
 
-    outputStream.write(nativeHandler(input, context));
-  }
+		outputStream.write(nativeHandler(input, context));
+	}
 
-  public static void main(String[] args) {
-    //Entrypoint ep = new Entrypoint();
-		//ByteArrayInputStream in = new ByteArrayInputStream("Dummy log".getBytes());
-		//// Dummy OutputStream that does absolutely nothing.
-		//OutputStream out = new OutputStream() { @Override public void write(int b) { } };
-    //ep.handle(in, out, null);
-  }
+	public static void main(String[] args) {
+	}
 
-  private static native byte[] nativeHandler(byte[] input, Context context);
+	private static native byte[] nativeHandler(byte[] input, Context context);
 }
