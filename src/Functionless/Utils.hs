4c5b5f65c782ecc3227e353ff9b013ab9674d583
Felix Raimundo <felix.raimundo@tweag.io>
2017-04-27 11:12:31 +0200

    Make the template compile properly

--- src/Functionless/Utils.hs
+++ src/Functionless/Utils.hs
@@ -1,17 +1,19 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Functionless.Utils (makeAwsLambdaHandler) where
 
 import qualified Data.ByteString as BS
-import qualified Language.Haskell.TH.Lib as TH
+import qualified Language.Haskell.TH as TH
+import qualified Language.Haskell.TH.Syntax as THS
 import Language.Java (J(..), JNIEnv, JClass, JType(..), reify, reflect)
 import Foreign.Ptr
 
 -- https://wiki.haskell.org/Template_Haskell#Wish_list
-makeAwsLambdaHandler :: TH.Name -> TH.Q [Dec]
+makeAwsLambdaHandler :: TH.Name -> TH.Q [TH.Dec]
 makeAwsLambdaHandler f = do
-    g <- TH.mkName "__functionlessWrapper"
+    let g = TH.mkName "__functionlessWrapper"
     ty <- [t| Ptr JNIEnv -> JClass -> J ('Array ('Prim "byte")) -> J ('Class "com.amazonaws.services.lambda.runtime.Context") -> IO (J ('Array ('Prim "byte"))) |]
-    TH.addTopDecls [TH.ForeignD (TH.ExportF TH.CCall "Java_io_tweag_functionless_Entrypoint_nativeHandler" g ty)]
-    sequence [TH.sigD g (return ty), TH.funD g (TH.clause [] (TH.normalB [| \_ _ input _ -> reify input >>= reflect . $(varE f) |]) [])]
+    THS.addTopDecls [TH.ForeignD (TH.ExportF TH.CCall "Java_io_tweag_functionless_Entrypoint_nativeHandler" g ty)]
+    sequence [TH.sigD g (return ty), TH.funD g [(TH.clause [] (TH.normalB [| \_ _ input _ -> reify input >>= reflect . $(TH.varE f) |]) [])]]
